1fd8bfa2dfda03f852fddbddbeb652b2c45a03b0
Emarginatus <emarginatus@muscardinus.be>
2016-02-17 22:47:20 +0100

    add convert method for Polygon

---
+++ R/convert_polygon.R
@@ -0,0 +1,42 @@
+#' Convert a spatial object
+#' @param object the object to convert
+#' @name convert
+#' @rdname convert
+#' @exportMethod convert
+#' @docType methods
+#' @importFrom methods setGeneric
+setGeneric(
+  name = "convert",
+  def = function(object){
+    standard.generic("convert")
+  }
+)
+
+#' @rdname convert
+#' @importFrom methods setMethod
+#' @importClassesFrom sp Polygon
+#' @importFrom digest sha1
+setMethod(
+  f = "convert",
+  signature = "Polygon",
+  definition = function(object){
+    hash <- sha1(list(Hole = object@hole, Coords = object@coords))
+    coordinates <- data.frame(
+      hash,
+      seq_len(nrow(object@coords)),
+      object@coords,
+      stringsAsFactors = FALSE
+    )
+    colnames(coordinates) <- c("Hash", "Order", "X", "Y")
+    return(
+      list(
+        Coordinates = coordinates,
+        Feature = data.frame(
+          Hash = hash,
+          Type = ifelse(object@hole, "H", "S"),
+          stringsAsFactors = FALSE
+        )
+      )
+    )
+  }
+)
