89530219d3dbaf7da8b24bfc298922cdecef692e
Emarginatus <emarginatus@muscardinus.be>
2016-02-18 22:48:16 +0100

    add combine()

---
+++ R/combine.R
@@ -0,0 +1,40 @@
+#' Combine objects
+#' @param ... the objects
+#' @name combine
+#' @rdname combine
+#' @exportMethod combine
+#' @docType methods
+#' @importFrom methods setGeneric
+setGeneric(
+  name = "combine",
+  def = function(...){
+    standard.generic("combine") #nocov
+  }
+)
+
+#' @rdname combine
+#' @importFrom methods setMethod
+#' @include geoversion_class.R
+setMethod(
+  f = "combine",
+  signature = "geoVersion",
+  definition = function(...){
+    coordinates <- lapply(
+      list(...),
+      function(x){
+        x@Coordinates
+      }
+    )
+    feature <- lapply(
+      list(...),
+      function(x){
+        x@Feature
+      }
+    )
+    new(
+      "geoVersion",
+      Coordinates = do.call(rbind, coordinates),
+      Feature = do.call(rbind, feature)
+    )
+  }
+)
