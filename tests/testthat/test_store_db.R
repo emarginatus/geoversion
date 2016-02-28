b3547e2cb7343f7b4bd9fc7103630927cf9a5404
Emarginatus <emarginatus@muscardinus.be>
2016-02-28 11:14:40 +0100

    store() doesn't change the database when adding the same geoVersion object twice

--- tests/testthat/test_store_db.R
+++ tests/testthat/test_store_db.R
@@ -45,3 +45,52 @@ test_that("it stores a geoVersion correctly in an empty database", {
 
   expect_identical(element$spawn, attributevalue$spawn)
 })
+test_that(
+  "no change when the geoVersion equals the current version in the database", {
+  timestamp <- as.numeric(Sys.time())
+  store(x = gv, connection = connection)
+  element <- dbReadTable(connection, "element")
+  expect_identical(
+    element %>% select_(~id, ~features),
+    gv@LayerElement
+  )
+  element <- element %>%
+    distinct_(~spawn, ~destroy)
+  expect_identical(nrow(element), 1L)
+  expect_false(is.na(element$spawn))
+  expect_true(is.na(element$destroy))
+  expect_less_than(element$spawn, timestamp)
+
+  features <- dbReadTable(connection, "features")
+  expect_identical(features, gv@Features)
+
+  feature <- dbReadTable(connection, "feature")
+  expect_identical(feature, gv@Feature)
+
+  coordinates <- dbReadTable(connection, "coordinates")
+  expect_identical(coordinates, gv@Coordinates)
+
+  attribute <- dbReadTable(connection, "attribute")
+  expect_identical(
+    attribute %>%
+      arrange_(~id),
+    gv@Attribute %>%
+      select_(~id, ~name, ~type) %>%
+      arrange_(~id)
+  )
+
+  attributevalue <- dbReadTable(connection, "attributevalue")
+  expect_identical(
+    attributevalue %>%
+      select_(~element, ~attribute, ~value),
+    gv@AttributeValue
+  )
+  attributevalue <- attributevalue %>%
+    distinct_(~spawn, ~destroy)
+  expect_identical(nrow(attributevalue), 1L)
+  expect_false(is.na(attributevalue$spawn))
+  expect_true(is.na(attributevalue$destroy))
+  expect_less_than(attributevalue$spawn, timestamp)
+
+  expect_identical(element$spawn, attributevalue$spawn)
+})
